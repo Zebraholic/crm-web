9c6e20066efbddc86e909eaa7698b2f9e34e2bdf
Zebraholic <rctavares@rogers.com>
2014-10-28 16:01:28 -0400

    Added contact form.

--- crm.rb
+++ crm.rb
@@ -3,16 +3,18 @@ require_relative 'contact'
 require 'sinatra'
 
 
+$rolodex= Rolodex.new
+
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get '/contacts' do
-	@contacts = []
-  @contacts << Contact.new("Rob", "Tavares", "rob@roberttavares.com", "Mad Props")
-  @contacts << Contact.new("Kill", "Bill", "kill@bill.com", "Co-Founder")
-  @contacts << Contact.new("Anne", "Hathaway", "anne@hathaway.com", "Instructor")
-
 	erb :contacts
 end
+
+get '/contacts/new' do 
+	erb :new_contact
+end
+
