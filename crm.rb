d171e582966f57fa5f13f96d98d95b599a2f504e
Zebraholic <rctavares@rogers.com>
2014-10-31 15:19:01 -0400

    Updated get and post contacts routes for SQLite database installation.

--- crm.rb
+++ crm.rb
@@ -25,36 +25,23 @@ get '/' do
 	erb :index
 end
 
+get "/contacts" do
+  @contacts = Contact.all
+  erb :contacts
+end
+
 get '/contacts/new' do 
 	erb :new_contact
 end
 
-get '/contacts' do
-	erb :contacts
-end
-
-get "/contacts/:id" do
-  @contact = $rolodex.find(params[:id].to_i)
-  if @contact
-    erb :show_contact
-  else
-    raise Sinatra::NotFound
-  end
-end
-
-post '/contacts' do
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
+post "/contacts" do
+  contact = Contact.create(
+    :first_name => params[:first_name],
+    :last_name => params[:last_name],
+    :email => params[:email],
+    :note => params[:note]
+  )
   redirect to('/contacts')
-end 
-
-get "/contacts/:id/edit" do
-  @contact = $rolodex.find(params[:id].to_i)
-  if @contact
-    erb :edit_contact
-  else
-    raise Sinatra::NotFound
-  end
 end
 
 put "/contacts/:id" do
